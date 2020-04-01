e315d8e2070dc036ac7ccfdf5951f7b04bea43c6
Mali <maliyudina@gmail.com>
2020-04-01 12:06:47 +0300

    delete chrome path (path to exe file)

--- test_framew/test_items/conftest.py
+++ test_framew/test_items/conftest.py
@@ -11,29 +11,27 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
-    chrome_path = r'/Users/Mali/Downloads/chromedriver'
-    # pdb.set_trace()
     language = request.config.getoption('language')
     if language == "ru":
         print("\nStart chrome languages ru for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': 'ru'})
-        browser = webdriver.Chrome(options=options, executable_path=chrome_path)
+        browser = webdriver.Chrome(options=options)
     elif language == "es":
         print("\nStart chrome languages en for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': 'es'})
-        browser = webdriver.Chrome(options=options, executable_path=chrome_path)
+        browser = webdriver.Chrome(options=options)
     elif language == "fr":
         print("\nStart chrome languages en for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': 'fr'})
-        browser = webdriver.Chrome(options=options, executable_path=chrome_path)
+        browser = webdriver.Chrome(options=options)
     elif language == "zh-ch":
         print("\nStart chrome languages en for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': 'zh-ch'})
-        browser = webdriver.Chrome(options=options, executable_path=chrome_path)
+        browser = webdriver.Chrome(options=options)
     else:
         print(f'Language "{language}" is not available. Please choose ru, es, fr or zh-ch.')
     yield browser
